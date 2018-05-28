df3a17005b49ca74bf1a82d4d6c8cd4f52254438
federicorosato98 <mrfeder1998@gmail.com>
2018-05-28 19:20:28 +0200

    finito

--- Convertitore_valuta/public_html/script.js
+++ Convertitore_valuta/public_html/script.js
@@ -1,15 +1,41 @@
 var valute = [];
 var xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function () {
-	if (this.readyState === 4 && this.status === 200) {
-		valute = JSON.parse(xhttp.response);
-	}
+window.onload = function () {
+    fill();
 };
-xhttp.open("GET", "myJson.json", true);
-xhttp.send();
 
-for (var i = 0; i < valute.length; i++) {
-	document.getElementById("val1").appendChild(new Option(valute[i]));
+function fill() {
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            valute = JSON.parse(xhttp.response);
+            var val1 = document.getElementById('val1');
+            var newOption1;
+            var val2 = document.getElementById('val2');
+            var newOption2;
+
+            for (var i = 0; i < valute.length; i++) {
+                newOption1 = document.createElement("option");
+                newOption2 = document.createElement("option");
+                newOption1.value = valute[i].tasso_EUR;
+                newOption1.innerHTML = valute[i].nomeItaliano + "\t(" + valute[i].sigla + ")";
+                newOption2.value = valute[i].tasso_EUR;
+                newOption2.innerHTML = valute[i].nomeItaliano + "\t(" + valute[i].sigla + ")";
+                val1.appendChild(newOption1);
+                val2.appendChild(newOption2);
+            }
+        }
+    };
+    xhttp.open("GET", "myJson.json", true);
+    xhttp.send();
 }
 
+function converti(input){
+    var before = input;
+    
+    if(input !== null){
+        input = input * document.getElementById('val1').value;
+        input = input / document.getElementById('val2').value;
+        document.getElementById("ris").value = Number(before).toFixed(2) + " = " + Number(input).toFixed(2);
+    }
+};
